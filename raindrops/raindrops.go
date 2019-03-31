52797842e93a35702de8aab1eb7ce078f952da40
mstranger <mr.mstranger@gmail.com>
2019-03-31 20:49:32 +0300

    Make Raindrops solution more simple and idiomatic

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,35 +1,21 @@
 package raindrops
 
-import "fmt"
-
-var raindrop = map[int]string{
-  3: "Pling",
-  5: "Plang",
-  7: "Plong",
-}
-
-// Factors returns all numbers that divided by given number
-func Factors(n int) (result []int) {
-  for i := 1; i <= n; i++ {
-    if n%i == 0 {
-      result = append(result, i)
-    }
-  }
-
-  return
-}
+import "strconv"
 
 // Convert change a number to a string
 func Convert(n int) (s string) {
-  for _, v := range Factors(n) {
-    if val, ok := raindrop[v]; ok {
-      s += val
-    }
-  }
+	if n%3 == 0 {
+		s += "Pling"
+	}
+	if n%5 == 0 {
+		s += "Plang"
+	}
+	if n%7 == 0 {
+		s += "Plong"
+	}
+	if s == "" {
+		s = strconv.Itoa(n)
+	}
 
-  if s == "" {
-    s = fmt.Sprintf("%v", n)
-  }
-
-  return
+	return
 }
