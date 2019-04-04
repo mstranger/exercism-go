62aa673de33f37a26a2ae8cca58c9f6365390a76
mstranger <mr.mstranger@gmail.com>
2019-04-04 18:33:20 +0300

    Solve task Isogram

---
+++ isogram/isogram.go
@@ -0,0 +1,22 @@
+package isogram
+
+import "unicode"
+
+// IsIsogram checks if word or phrase is an isogram.
+func IsIsogram(s string) bool {
+	var hash = map[rune]int{}
+
+	for _, c := range s {
+		if c == ' ' || c == '-' {
+			continue
+		}
+
+		if hash[unicode.ToLower(c)] == 1 {
+			return false
+		}
+
+		hash[unicode.ToLower(c)]++
+	}
+
+	return true
+}
