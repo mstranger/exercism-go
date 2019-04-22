9c41a3cb72da8bfae44a13fbfff962590adedca3
mstranger <mr.mstranger@gmail.com>
2019-04-22 12:06:22 +0300

    Edit Luhn solution. Delete second loop and function.

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -7,40 +7,31 @@ import (
 
 // Valid determines if given number is valid or not per the Luhn formula.
 func Valid(s string) bool {
-	s, err := transformString(s)
+	var sum int
 
-	if err != nil || len(s) == 1 {
+	s = strings.ReplaceAll(s, " ", "")
+
+	if len(s) == 1 {
 		return false
 	}
 
-	var sum int
-	for _, v := range s {
-		n, _ := strconv.Atoi(string(v))
+	for i, j := len(s)-1, 0; i >= 0; i, j = i-1, j+1 {
+		n, err := strconv.Atoi(string(s[i]))
+		if err != nil {
+			return false
+		}
+
+		// every second, starting from the right
+		if j&1 == 1 {
+			if 2*n > 9 {
+				n = 2*n - 9
+			} else {
+				n = 2 * n
+			}
+		}
+
 		sum += n
 	}
 
 	return sum%10 == 0
-}
-
-// double every second digit, starting from the right.
-// delete all spaces. return error if any non-digit character found.
-func transformString(s string) (string, error) {
-	s = strings.ReplaceAll(s, " ", "")
-
-	_, err := strconv.Atoi(s)
-	if err != nil {
-		return "", err
-	}
-
-	for i := len(s) - 2; i >= 0; i -= 2 {
-		n, _ := strconv.Atoi(string(s[i]))
-		if n > 9 {
-			n = 2*n - 9
-		} else {
-			n = 2 * n
-		}
-		s = s[:i] + strconv.Itoa(n) + s[i+1:] // replace s[i]
-	}
-
-	return s, nil
 }
