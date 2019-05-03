3a5c4686a5adf4358dbbd8ada19dc8cf00763e55
mstranger <mr.mstranger@gmail.com>
2019-05-03 13:18:46 +0300

    Edit Secret Handshake. Use iota

--- secret-handshake/secret_handshake.go
+++ secret-handshake/secret_handshake.go
@@ -1,25 +1,30 @@
 package secret
 
-// decimal equivalents of the 1, 10, 100, 1000 and 10000 in binary
-var codes = [5]uint{1, 2, 4, 8, 16}
-var values = [5]string{"wink", "double blink", "close your eyes", "jump", ""}
+const (
+	wink uint = 1 << iota
+	doubleBlink
+	closeYourEyes
+	jump
+	reversed
+)
 
 // Handshake converts a dicimal number to the appropriate sequence of events.
 func Handshake(n uint) []string {
 	result := make([]string, 0)
 
-	for i, v := range codes[:4] {
-		// for example:
-		// 1111 & 1000 = 1000 in binary; 15 & 8 == 8 in decimal
-		if n&v == 0 {
-			continue
-		}
-
-		result = append(result, values[i])
+	if n&wink != 0 {
+		result = append(result, "wink")
 	}
-
-	// reverse array if addition of 16
-	if n&codes[4] != 0 {
+	if n&doubleBlink != 0 {
+		result = append(result, "double blink")
+	}
+	if n&closeYourEyes != 0 {
+		result = append(result, "close your eyes")
+	}
+	if n&jump != 0 {
+		result = append(result, "jump")
+	}
+	if n&reversed != 0 {
 		reverse(result)
 	}
 
