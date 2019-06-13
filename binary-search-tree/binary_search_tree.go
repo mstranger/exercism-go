281587a6d5cbfaece6752a2ff20436c18f153731
mstranger <mr.mstranger@gmail.com>
2019-06-13 20:40:46 +0300

    Edit and comment Binary Search tree task

--- binary-search-tree/binary_search_tree.go
+++ binary-search-tree/binary_search_tree.go
@@ -1,35 +1,32 @@
-// package main
 package binarysearchtree
 
-import (
-	"sort"
-)
-
+// SearchTreeData implements Binary Search Tree structure.
 type SearchTreeData struct {
 	data  int
 	left  *SearchTreeData
 	right *SearchTreeData
 }
 
+// Bst creates new tree.
 func Bst(n int) *SearchTreeData {
 	return &SearchTreeData{data: n}
 }
 
+// Insert inserts a given number to the binary tree.
 func (t *SearchTreeData) Insert(n int) {
-	if t == nil {
-		return
-	}
+	// insert left
 	if t.data >= n {
 		if t.left == nil {
-			t.left = &SearchTreeData{data: n}
+			t.left = Bst(n)
 			return
 		} else {
 			t.left.Insert(n)
 		}
 	}
+	// insert right
 	if t.data < n {
 		if t.right == nil {
-			t.right = &SearchTreeData{data: n}
+			t.right = Bst(n)
 			return
 		} else {
 			t.right.Insert(n)
@@ -37,6 +34,8 @@ func (t *SearchTreeData) Insert(n int) {
 	}
 }
 
+// MapStrings returns a sorted array of string type.
+// Calls the function f for each element of the tree.
 func (t *SearchTreeData) MapString(f func(int) string) []string {
 	arr := make([]interface{}, 0)
 	traverse(t, &arr)
@@ -46,13 +45,11 @@ func (t *SearchTreeData) MapString(f func(int) string) []string {
 		res[i] = f(v.(int))
 	}
 
-	sort.Slice(res, func(i, j int) bool {
-		return res[i] < res[j]
-	})
-
 	return res
 }
 
+// MapInt return a sorted array of int type.
+// Calls the function f for each element of the tree.
 func (t *SearchTreeData) MapInt(f func(int) int) []int {
 	arr := make([]interface{}, 0)
 	traverse(t, &arr)
@@ -62,35 +59,16 @@ func (t *SearchTreeData) MapInt(f func(int) int) []int {
 		res[i] = f(v.(int))
 	}
 
-	sort.Slice(res, func(i, j int) bool {
-		return res[i] < res[j]
-	})
-
 	return res
 }
 
+// LNR tree traversal (in-order)
+// returns data in sorted order
 func traverse(t *SearchTreeData, arr *[]interface{}) {
 	if t == nil {
 		return
 	}
-	*arr = append(*arr, t.data)
 	traverse(t.left, arr)
+	*arr = append(*arr, t.data)
 	traverse(t.right, arr)
-	return
 }
-
-// func main() {
-// 	t := Bst(4)
-// 	t.Insert(2)
-// 	t.Insert(6)
-// 	t.Insert(3)
-// 	t.Insert(1)
-// 	t.Insert(5)
-// 	t.Insert(7)
-
-// 	arr1 := t.MapInt(func(x int) int { return x })
-// 	arr2 := t.MapString(strconv.Itoa)
-
-// 	fmt.Printf("%v\n", arr1)
-// 	fmt.Printf("%q\n", arr2)
-// }
