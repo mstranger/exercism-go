16b78f047912aa43076fefffcf220c314072e31f
mstranger <mr.mstranger@gmail.com>
2019-06-13 16:47:45 +0300

    Binary Search. Second variant

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -2,30 +2,20 @@ package binarysearch
 
 // SearchInts implements the binary search algorithm.
 func SearchInts(arr []int, key int) int {
-	var middle int
-	var found bool
-	var i int
-	for len(arr) > 0 {
-		middle = len(arr) / 2
+	min, max := 0, len(arr)-1
+
+	middle := (min + max) / 2
+	for min <= max {
 		if arr[middle] == key {
-			i += middle
-			found = true
-			break
+			return middle
+		} else if arr[middle] > key {
+			max = middle - 1
+		} else {
+			min = middle + 1
 		}
-		if arr[middle] > key {
-			arr = arr[:middle]
-			continue
-		}
-		if arr[middle] < key {
-			i += middle + 1
-			arr = arr[middle+1:]
-			continue
-		}
+
+		middle = (min + max) / 2
 	}
 
-	if !found {
-		i = -1
-	}
-
-	return i
+	return -1
 }
