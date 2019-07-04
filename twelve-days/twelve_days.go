da2017e98d37dd0e9a0b9791c065290bb0ea8193
mstranger <mr.mstranger@gmail.com>
2019-07-04 15:16:45 +0300

    Edit and refactor Twelve Days solution

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -7,22 +7,22 @@ import (
 )
 
 type twelveDays struct {
-	DayString, GiftCount, Gift string
+	DayString, Gift string
 }
 
 var gifts = []twelveDays{
-	{"first", "a", "Partridge"},
-	{"second", "two", "Turtle Doves"},
-	{"third", "three", "French Hens"},
-	{"fourth", "four", "Calling Birds"},
-	{"fifth", "five", "Gold Rings"},
-	{"sixth", "six", "Geese-a-Laying"},
-	{"seventh", "seven", "Swans-a-Swimming"},
-	{"eighth", "eight", "Maids-a-Milking"},
-	{"ninth", "nine", "Ladies Dancing"},
-	{"tenth", "ten", "Lords-a-Leaping"},
-	{"eleventh", "eleven", "Pipers Piping"},
-	{"twelfth", "twelve", "Drummers Drumming"},
+	{"first", "a Partridge"},
+	{"second", "two Turtle Doves"},
+	{"third", "three French Hens"},
+	{"fourth", "four Calling Birds"},
+	{"fifth", "five Gold Rings"},
+	{"sixth", "six Geese-a-Laying"},
+	{"seventh", "seven Swans-a-Swimming"},
+	{"eighth", "eight Maids-a-Milking"},
+	{"ninth", "nine Ladies Dancing"},
+	{"tenth", "ten Lords-a-Leaping"},
+	{"eleventh", "eleven Pipers Piping"},
+	{"twelfth", "twelve Drummers Drumming"},
 }
 
 // Song outputs the lirics "The Twelve Days of Christmas".
@@ -45,24 +45,18 @@ func Verse(n int) string {
 		"On the %s day of Christmas my true love gave to me: %s in a Pear Tree.",
 		gifts[n-1].DayString, giftsOnDay(n))
 
+	if n == 1 {
+		s = strings.Replace(s, " and", "", -1)
+	}
+
 	return s
 }
 
 // return a list of all gifts for the day
 func giftsOnDay(n int) string {
 	if n == 1 {
-		return gifts[0].GiftCount + " " + gifts[0].Gift
+		return "and " + gifts[0].Gift
 	}
 
-	return giftsExceptFirst(n) + "and " + giftsOnDay(1)
-}
-
-// return all gifts for the day except first
-func giftsExceptFirst(n int) string {
-	if n == 1 {
-		return ""
-	}
-
-	return strings.Join([]string{gifts[n-1].GiftCount + " " + gifts[n-1].Gift,
-		giftsExceptFirst(n - 1)}, ", ")
+	return strings.Join([]string{gifts[n-1].Gift, giftsOnDay(n - 1)}, ", ")
 }
