4ab18cce34880bd10f8db8ae79555bb5e471bd2d
mstranger <mr.mstranger@gmail.com>
2019-12-05 18:15:16 +0200

    Solve Flatten Array task.

--- flatten-array/flatten_array.go
+++ flatten-array/flatten_array.go
@@ -1,1 +1,26 @@
 package flatten
+
+import "reflect"
+
+// Flatten takes a nested list and returns a single flattened list
+// with all values except nil.
+func Flatten(nested interface{}) []interface{} {
+	result := []interface{}{}
+
+	switch reflect.TypeOf(nested).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(nested)
+
+		for i := 0; i < s.Len(); i++ {
+			if s.Index(i).Interface() == nil {
+				continue
+			}
+			t := s.Index(i).Interface().(interface{})
+			result = append(result, Flatten(t)...)
+		}
+	default:
+		result = append(result, nested)
+	}
+
+	return result
+}
