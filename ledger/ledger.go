c81b4b58ca0e663c7f569f362ecf69fd2d58ec25
mstranger <mr.mstranger@gmail.com>
2019-12-09 12:54:09 +0200

    Write parseEntry func.

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -7,9 +7,17 @@ import (
 )
 
 type Entry struct {
-	Date        string // "Y-m-d"
+	// "Y-m-d"
+	Date        string
 	Description string
-	Change      int // in cents
+	// in cents
+	Change int
+}
+
+type chunk struct {
+	i int
+	s string
+	e error
 }
 
 func FormatLedger(currency string, locale string, entries []Entry) (string, error) {
@@ -46,155 +54,10 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 	}
 
 	// Parallelism, always a great idea
-	co := make(chan struct {
-		i int
-		s string
-		e error
-	})
+	co := make(chan chunk)
 
 	for i, et := range entriesCopy {
-		go func(i int, entry Entry) {
-			if len(entry.Date) != 10 {
-				co <- struct {
-					i int
-					s string
-					e error
-				}{e: errors.New("")}
-			}
-			d1, d2, d3, d4, d5 := entry.Date[0:4], entry.Date[4], entry.Date[5:7], entry.Date[7], entry.Date[8:10]
-			if d2 != '-' {
-				co <- struct {
-					i int
-					s string
-					e error
-				}{e: errors.New("")}
-			}
-			if d4 != '-' {
-				co <- struct {
-					i int
-					s string
-					e error
-				}{e: errors.New("")}
-			}
-			de := entry.Description
-			if len(de) > 25 {
-				de = de[:22] + "..."
-			} else {
-				de = de + strings.Repeat(" ", 25-len(de))
-			}
-			var d string
-			if locale == "nl-NL" {
-				d = d5 + "-" + d3 + "-" + d1
-			} else if locale == "en-US" {
-				d = d3 + "/" + d5 + "/" + d1
-			}
-			negative := false
-			cents := entry.Change
-			if cents < 0 {
-				cents = cents * -1
-				negative = true
-			}
-			var a string
-			if locale == "nl-NL" {
-				if currency == "EUR" {
-					a += "€"
-				} else if currency == "USD" {
-					a += "$"
-				} else {
-					co <- struct {
-						i int
-						s string
-						e error
-					}{e: errors.New("")}
-				}
-				a += " "
-				centsStr := strconv.Itoa(cents)
-				switch len(centsStr) {
-				case 1:
-					centsStr = "00" + centsStr
-				case 2:
-					centsStr = "0" + centsStr
-				}
-				rest := centsStr[:len(centsStr)-2]
-				var parts []string
-				for len(rest) > 3 {
-					parts = append(parts, rest[len(rest)-3:])
-					rest = rest[:len(rest)-3]
-				}
-				if len(rest) > 0 {
-					parts = append(parts, rest)
-				}
-				for i := len(parts) - 1; i >= 0; i-- {
-					a += parts[i] + "."
-				}
-				a = a[:len(a)-1]
-				a += ","
-				a += centsStr[len(centsStr)-2:]
-				if negative {
-					a += "-"
-				} else {
-					a += " "
-				}
-			} else if locale == "en-US" {
-				if negative {
-					a += "("
-				}
-				if currency == "EUR" {
-					a += "€"
-				} else if currency == "USD" {
-					a += "$"
-				} else {
-					co <- struct {
-						i int
-						s string
-						e error
-					}{e: errors.New("")}
-				}
-				centsStr := strconv.Itoa(cents)
-				switch len(centsStr) {
-				case 1:
-					centsStr = "00" + centsStr
-				case 2:
-					centsStr = "0" + centsStr
-				}
-				rest := centsStr[:len(centsStr)-2]
-				var parts []string
-				for len(rest) > 3 {
-					parts = append(parts, rest[len(rest)-3:])
-					rest = rest[:len(rest)-3]
-				}
-				if len(rest) > 0 {
-					parts = append(parts, rest)
-				}
-				for i := len(parts) - 1; i >= 0; i-- {
-					a += parts[i] + ","
-				}
-				a = a[:len(a)-1]
-				a += "."
-				a += centsStr[len(centsStr)-2:]
-				if negative {
-					a += ")"
-				} else {
-					a += " "
-				}
-			} else {
-				co <- struct {
-					i int
-					s string
-					e error
-				}{e: errors.New("")}
-			}
-			var al int
-			for range a {
-				al++
-			}
-			co <- struct {
-				i int
-				s string
-				e error
-			}{i: i, s: d + strings.Repeat(" ", 10-len(d)) + " | " + de + " | " +
-				strings.Repeat(" ", 13-al) + a + "\n"}
-		}(i, et)
+		go parseEntry(i, et, locale, currency, co)
 	}
 	ss := make([]string, len(entriesCopy))
 	for range entriesCopy {
@@ -204,9 +67,11 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 		}
 		ss[v.i] = v.s
 	}
+
 	for i := 0; i < len(entriesCopy); i++ {
 		s += ss[i]
 	}
+
 	return s, nil
 }
 
@@ -237,4 +102,119 @@ func translateLedger(locale string) (string, bool) {
 	default:
 		return "", false
 	}
+}
+
+func parseEntry(i int, entry Entry, locale, currency string, co chan<- chunk) {
+	if len(entry.Date) != 10 {
+		co <- chunk{e: errors.New("")}
+	}
+	d1, d2, d3, d4, d5 := entry.Date[0:4], entry.Date[4], entry.Date[5:7], entry.Date[7], entry.Date[8:10]
+	if d2 != '-' {
+		co <- chunk{e: errors.New("")}
+	}
+	if d4 != '-' {
+		co <- chunk{e: errors.New("")}
+	}
+	de := entry.Description
+	if len(de) > 25 {
+		de = de[:22] + "..."
+	} else {
+		de = de + strings.Repeat(" ", 25-len(de))
+	}
+	var d string
+	if locale == "nl-NL" {
+		d = d5 + "-" + d3 + "-" + d1
+	} else if locale == "en-US" {
+		d = d3 + "/" + d5 + "/" + d1
+	}
+	negative := false
+	cents := entry.Change
+	if cents < 0 {
+		cents = cents * -1
+		negative = true
+	}
+	var a string
+	if locale == "nl-NL" {
+		if currency == "EUR" {
+			a += "€"
+		} else if currency == "USD" {
+			a += "$"
+		} else {
+			co <- chunk{e: errors.New("")}
+		}
+		a += " "
+		centsStr := strconv.Itoa(cents)
+		switch len(centsStr) {
+		case 1:
+			centsStr = "00" + centsStr
+		case 2:
+			centsStr = "0" + centsStr
+		}
+		rest := centsStr[:len(centsStr)-2]
+		var parts []string
+		for len(rest) > 3 {
+			parts = append(parts, rest[len(rest)-3:])
+			rest = rest[:len(rest)-3]
+		}
+		if len(rest) > 0 {
+			parts = append(parts, rest)
+		}
+		for i := len(parts) - 1; i >= 0; i-- {
+			a += parts[i] + "."
+		}
+		a = a[:len(a)-1]
+		a += ","
+		a += centsStr[len(centsStr)-2:]
+		if negative {
+			a += "-"
+		} else {
+			a += " "
+		}
+	} else if locale == "en-US" {
+		if negative {
+			a += "("
+		}
+		if currency == "EUR" {
+			a += "€"
+		} else if currency == "USD" {
+			a += "$"
+		} else {
+			co <- chunk{e: errors.New("")}
+		}
+		centsStr := strconv.Itoa(cents)
+		switch len(centsStr) {
+		case 1:
+			centsStr = "00" + centsStr
+		case 2:
+			centsStr = "0" + centsStr
+		}
+		rest := centsStr[:len(centsStr)-2]
+		var parts []string
+		for len(rest) > 3 {
+			parts = append(parts, rest[len(rest)-3:])
+			rest = rest[:len(rest)-3]
+		}
+		if len(rest) > 0 {
+			parts = append(parts, rest)
+		}
+		for i := len(parts) - 1; i >= 0; i-- {
+			a += parts[i] + ","
+		}
+		a = a[:len(a)-1]
+		a += "."
+		a += centsStr[len(centsStr)-2:]
+		if negative {
+			a += ")"
+		} else {
+			a += " "
+		}
+	} else {
+		co <- chunk{e: errors.New("")}
+	}
+	var al int
+	for range a {
+		al++
+	}
+	co <- chunk{i: i, s: d + strings.Repeat(" ", 10-len(d)) + " | " + de + " | " +
+		strings.Repeat(" ", 13-al) + a + "\n"}
 }
